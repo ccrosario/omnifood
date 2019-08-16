dd459da023c8bd4060e29a9f30cd7852f9eccf47
Cesar Rosario <cesar.c.rosario@gmail.com>
2019-08-16 07:51:34 -0400

    added icons to mobile nav, working on jquery to change icons on open and close

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,22 +1,77 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$('.js--section-features').waypoint(function(direction) {
+    $('.js--section-features').waypoint(function (direction) {
 
-		if (direction == "down") {
+        if (direction == "down") {
 
-			$('nav').addClass('sticky');
+            $('nav').addClass('sticky');
 
-		} else {
+        } else {
 
-			$('nav').removeClass('sticky');
+            $('nav').removeClass('sticky');
 
-		}
+        }
 
-	}, {
+    }, {
 
-		offset: '60px;'
+            offset: '60px;'
 
-	});
+        });
 
-})
+    /*------------------Animations on Scroll--------------------*/
+
+    $('.js--wp-1').waypoint(function (direction) {
+
+        $('.js--wp-1').addClass('animated fadeIn');
+
+    }, {
+            offset: '50%'
+    })
+
+    $('.js--wp-2').waypoint(function (direction) {
+
+        $('.js--wp-2').addClass('animated fadeInUp');
+
+    }, {
+            offset: '50%'
+    })
+
+    $('.js--wp-3').waypoint(function (direction) {
+
+        $('.js--wp-3').addClass('animated fadeIn');
+
+    }, {
+            offset: '50%'
+    })
+
+    $('.js--wp-4').waypoint(function (direction) {
+
+        $('.js--wp-4').addClass('animated pulse');
+
+    }, {
+            offset: '50%'
+    })
+
+    /*------------------Mobile on Nav--------------------*/
+
+    $('.js--mobile-nav-icon').click(function () {
+
+        var nav = $('.js--main-nav');
+        var icon = $('.js--mobile-nav-icon ion-icon');
+
+        nav.slideToggle(200);
+
+        if (icon.has('[name="menu"]')) {
+
+            icon.replaceWith("<ion-icon name=\"close\"></ion-icon>");
+
+        } else if (icon.has('[name="close"]')) {
+
+            icon.replaceWith("<ion-icon name=\"menu\"></ion-icon>");
+
+        };
+
+    });
+
+});
